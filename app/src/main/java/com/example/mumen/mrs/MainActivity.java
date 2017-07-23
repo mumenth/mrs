022301e5a0ef22dd21b13076d5f5980f96e8676c
Hatem <hatem200011@gmail.com>
2017-07-23 12:56:06 -0700

    Hatem 24/7

--- app/src/main/java/com/example/mumen/mrs/MainActivity.java
+++ app/src/main/java/com/example/mumen/mrs/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mumen.mrs;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choices_2);
+
+    }
+
+    protected void GoToSignUpPage(View view) {
+        Context context = MainActivity.this;
+        Class destination = signup.class;
+        Intent intent = new Intent(context, destination);
+        startActivity(intent);
+
     }
 }
