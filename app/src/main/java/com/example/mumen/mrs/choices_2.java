e82747a59cf4333f129f452c2448e8d1c441caac
Hatem <hatem200011@gmail.com>
2017-07-23 15:20:12 -0700

    test123

--- app/src/main/java/com/example/mumen/mrs/choices_2.java
+++ app/src/main/java/com/example/mumen/mrs/choices_2.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class choices_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void GoToSignUpPage(View view) {
-        Context context = MainActivity.this;
+        Context context = choices_2.this;
         Class destination = signup.class;
         Intent intent = new Intent(context, destination);
         startActivity(intent);
