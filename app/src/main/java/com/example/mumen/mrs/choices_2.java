5cd8195806c802970be2f53b3eb2863077e263db
mumen thabet <mumen.thabet@gmail.com  git config --global user.name Your>
2017-07-24 15:26:46 +0300

    update layout

--- app/src/main/java/com/example/mumen/mrs/choices_2.java
+++ app/src/main/java/com/example/mumen/mrs/choices_2.java
@@ -3,7 +3,7 @@ package com.example.mumen.mrs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class main extends AppCompatActivity {
+public class choices_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
