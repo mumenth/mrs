a62b45101eb4acc41dfbff0bc0ea210e4ded6634
Hatem <hatem200011@gmail.com>
2017-07-25 14:04:55 -0700

    Hatem: the data_entry layout is add and signup and login are working

--- app/src/main/java/com/example/mumen/mrs/enter_data.java
+++ app/src/main/java/com/example/mumen/mrs/enter_data.java
@@ -2,6 +2,9 @@ package com.example.mumen.mrs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class enter_data extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class enter_data extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_data);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.save) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/mumen/mrs/signup.java
+++ app/src/main/java/com/example/mumen/mrs/signup.java
@@ -2,11 +2,14 @@ package com.example.mumen.mrs;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.example.mumen.mrs.databinding.SignupBinding;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
@@ -14,68 +17,52 @@ import com.parse.SignUpCallback;
 
 
 public class signup extends AppCompatActivity {
+    SignupBinding mBinding;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.signup);
+
+
     }
-    
-    private void loginInformationSubmitted() throws ParseException {
-//        String email = mBinding.etEmail.getText().toString();
-//        String password = mBinding.etPassword.getText().toString();
-//        if (mBinding.switchSignupToggle.isChecked()) {
-//            signUp(email, password);
-//        } else {
-//            logIn(email, password);
-//        }
-    }
-
-    private void signUp(String email, String password) {
-        ParseUser user = new ParseUser();
-        user.setUsername(email);
-        user.setEmail(email);
-        user.setPassword(password);
-
-        Toast.makeText(getApplicationContext(), "Signing up...", Toast.LENGTH_SHORT).show();
-
-        user.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException exception) {
-                if (exception == null) {
-                    loginSuccess();
-                } else {
-                    Log.d("signup", "Error: " + exception.getMessage());
-
-                    Toast.makeText(getApplicationContext(), "Signup failed :[\nPlease try again.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }{
-        ParseUser user = new ParseUser();
-        user.setUsername(email);
-        user.setEmail(email);
-        user.setPassword(password);
-
-        Toast.makeText(getApplicationContext(), "Signing up...", Toast.LENGTH_SHORT).show();
-
-        user.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException exception) {
-                if (exception == null) {
-                    loginSuccess();
-                } else {
-                    Log.d("signup", "Error: " + exception.getMessage());
-
-                    Toast.makeText(getApplicationContext(), "Signup failed :[\nPlease try again.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
     private void loginSuccess() {
         Context context = signup.this;
         Class destinationActivity = login.class;
         Intent intent = new Intent(context, destinationActivity);
         startActivity(intent);
     }
+
+    public void onSignupButtonClicked(View view) {
+        String email = mBinding.etEmail.getText().toString();
+        String password = mBinding.etPassword.getText().toString();
+        signUp(email , password);
+    }
+
+    public void signUp (String email, String password){
+        ParseUser user = new ParseUser();
+        user.setEmail(email);
+        user.setPassword(password);
+
+        Toast.makeText(getApplicationContext(), "Signing up...", Toast.LENGTH_SHORT).show();
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException exception) {
+                if (exception == null) {
+                    loginSuccess();
+                } else {
+                    Log.d("signup", "Error: " + exception.getMessage());
+
+                    Toast.makeText(getApplicationContext(), "Signup failed :[\nPlease try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
 }
+
