98e79f9841f35928241c90b79ebfa60bdb5d4608
mumen thabet <mumen.thabet@gmail.com  git config --global user.name Your>
2017-07-26 16:23:50 +0300

    menu

--- app/src/main/java/com/example/mumen/mrs/ready_to_go.java
+++ app/src/main/java/com/example/mumen/mrs/ready_to_go.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ready_to_go extends AppCompatActivity {
@@ -18,4 +21,38 @@ public class ready_to_go extends AppCompatActivity {
         Class destination = enter_data.class;
         Intent intent = new Intent(context, destination);
         startActivity(intent);}
+    public void export_activity(View view) {
+        Context context = ready_to_go.this;
+        Class destination = export.class;
+        Intent intent = new Intent(context, destination);
+        startActivity(intent);
+    }
+    public void developer_page(View view) {
+        Context context = ready_to_go.this;
+        Class destination = activity_main8.class;
+        Intent intent = new Intent(context, destination);
+        startActivity(intent);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.export) {
+            export_activity(null);
+            return true;
+        }else if (id == R.id.developer){
+            developer_page(null);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/mumen/mrs/welcome.java
+++ app/src/main/java/com/example/mumen/mrs/welcome.java
@@ -23,39 +23,6 @@ public class welcome extends AppCompatActivity {
         Intent intent = new Intent(context, destination);
         startActivity(intent);
     }
-    public void export_activity(View view) {
-        Context context = welcome.this;
-        Class destination = export.class;
-        Intent intent = new Intent(context, destination);
-        startActivity(intent);
-    }
-    public void developer_page(View view) {
-        Context context = welcome.this;
-        Class destination = activity_main8.class;
-        Intent intent = new Intent(context, destination);
-        startActivity(intent);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_bar, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.export) {
-            export_activity(null);
-            return true;
-        }else if (id == R.id.developer){
-            developer_page(null);
-            return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
 
 }
